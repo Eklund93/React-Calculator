2fb1f781f33f1b3538c31c9a1d2c3a04e01a465e
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2019-05-20 19:58:53 +0200

    Added a few tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import  { mount } from 'enzyme';
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    mount(<App />);
 });
+
+
+it('does addition', () => {
+    const wrapper = mount(<App />);
+
+    wrapper.find('#four').simulate('click');
+    wrapper.find('#add').simulate('click');
+    wrapper.find('#three').simulate('click');
+    wrapper.find('#equals').simulate('click');
+
+    expect(wrapper.find('.input').text()).toBe('7');
+});
+
+it('does subtraction', () => {
+    const wrapper = mount(<App />);
+
+    wrapper.find('#nine').simulate('click');
+    wrapper.find('#minus').simulate('click');
+    wrapper.find('#one').simulate('click');
+    wrapper.find('#equals').simulate('click');
+
+    expect(wrapper.find('.input').text()).toBe('8');
+});
+
+it('does multiplication', () => {
+    const wrapper = mount(<App />);
+
+    wrapper.find('#two').simulate('click');
+    wrapper.find('#five').simulate('click');
+    wrapper.find('#times').simulate('click');
+    wrapper.find('#one').simulate('click');
+    wrapper.find('#equals').simulate('click');
+
+    expect(wrapper.find('.input').text()).toBe('25');
+});
+
+it('does division', () => {
+    const wrapper = mount(<App />);
+
+    wrapper.find('#two').simulate('click');
+    wrapper.find('#divide').simulate('click');
+    wrapper.find('#one').simulate('click');
+    wrapper.find('#equals').simulate('click');
+
+    expect(wrapper.find('.input').text()).toBe('2');
+});
+
+it('does not crash when dividing by zero', () => {
+    const wrapper = mount(<App />);
+
+    wrapper.find('#two').simulate('click');
+    wrapper.find('#divide').simulate('click');
+    wrapper.find('#zero').simulate('click');
+    wrapper.find('#equals').simulate('click');
+
+    expect(wrapper.find('.input').text()).toBe('NaN');
+});
+
